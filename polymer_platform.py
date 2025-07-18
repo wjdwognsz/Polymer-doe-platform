ae6c03e94164b2768478c8ada22287b9257a74c5
wjdwognsz <wjdwognsz@gmail.com>
2025-07-18 16:44:07 +0900

    Update polymer_platform.py

--- polymer_platform.py
+++ polymer_platform.py
@@ -1951,6 +1951,7 @@ class DatabaseManager:
             'data': results
         }
 
+database_manager = DatabaseManager()
 
 # ==================== 번역 서비스 ====================
 class TranslationService:
